001e45d453825680a73c2b38937ee4c361eb87b7
Tayyaba-Iram <a.tayyaba786@gmail.com>
2024-09-21 14:55:55 +0500

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,9 @@
-import Navbar from "./Navbar"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "./Home"
-import { createContext, useEffect, useState } from "react"
-import axios from "axios"
-import Footer from "./Footer.jsx"
+import { createContext } from "react"
 export const userContext = createContext();
 
 function App() {
-  const [user, setUser] = useState({
-    username: null,
-    email: null
-  })
-  axios.defaults.withCredentials = true;
-  useEffect(() => {
-    axios.get('http://localhost:3001/')
-    .then(user => {
-      setUser(user.data)
-    })
-    .catch(err => console.log(err))
-  }, [])
+  
 
   return (
     <userContext.Provider value={user}>
