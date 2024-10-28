c7a700043fdd1d3350be6ea0a0e7fef1e2af1659
tayyabakhan6776 <tayyabakhan7070@gmail.com>
2024-10-28 11:23:04 +0500

    ScheduleSession

---
+++ frontend/src/ScheduleSession.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ScheduleSession() {
+  return (
+    <div>
+         <div className='ScheduleSession'>
+            
+         </div>
+      
+    </div>
+  )
+}
